d1ba361ccb58fea749ca16758a30e995eab86f34
William Imoukhuede <wimoukhuede@gmail.com>
2021-06-23 23:51:48 +0100

    Using a different response

--- routes/fundme.js
+++ routes/fundme.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 const undergraduate = require("../routes/undergraduates");
 
 router.get('/', (req, res) => {
-    res.status(200).json({message: 'It works'})
+    res.send('Hello, World!')
+    //res.status(200).json({message: 'It works'});
 })
+
 router.use("/", undergraduate);
 
 module.exports = router;
