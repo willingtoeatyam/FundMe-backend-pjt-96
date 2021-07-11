81b82a2c5b38681996e8fdc6e65585838f01e41e
William Imoukhuede <wimoukhuede@gmail.com>
2021-07-11 19:52:50 +0100

    moved into app folder

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,8 @@
-var path = require("path");
 // const fundme = require("./webapp/fundme");
 const mobilefundme = require("./mobile/fundme");
 
 module.exports = (app) => {
-  var Router = app.Router();
-  var router = new Router(path.join(__dirname,'routes'));
   // app.use("/api/v1/webapp/fundme", fundme);
-  router.use("/api/v1/mobileapp/fundme", mobilefundme);
+  app.use("/api/v1/mobileapp/fundme", mobilefundme);
   // app.use("/api/v2/fund") for a newer version of api;
 };
