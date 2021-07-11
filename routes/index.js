325e71799ebe876a0209bcd0717fd1bd91d7fb4b
William Imoukhuede <wimoukhuede@gmail.com>
2021-07-11 19:43:25 +0100

    router change

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
+var path = require("path");
 // const fundme = require("./webapp/fundme");
 const mobilefundme = require("./mobile/fundme");
 
 module.exports = (app) => {
+  var Router = app.Router();
+  var router = new Router(path.join(__dirname,'routes'));
   // app.use("/api/v1/webapp/fundme", fundme);
-  app.use("/api/v1/mobileapp/fundme", mobilefundme);
+  router.use("/api/v1/mobileapp/fundme", mobilefundme);
   // app.use("/api/v2/fund") for a newer version of api;
 };
